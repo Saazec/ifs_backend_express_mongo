597322ab7b73d0ab451da9a5f44d4c689f1c7bbf
Salman Rehmatullah <salman.r@sysforetech.local>
2019-02-21 16:02:30 +0530

    added authentication and changed controllers implementation

--- controllers/users.js
+++ controllers/users.js
@@ -6,89 +6,128 @@ const User = require('../models/users');
 const connUri = process.env.MONGO_LOCAL_CONN_URL;
 
 module.exports = {
+    // add: (req, res) => {
+    //     mongoose.connect(connUri, { useNewUrlParser: true }, err => {
+    //         let result = {};
+    //         let status = 201;
+    //         if (!err) {
+    //             const { name, password } = req.body;
+    //             const user = new User({ name, password });
+    //             user.save((err, user) => {
+    //                 if (!err) {
+    //                     result.status = status;
+    //                     result.result = user;
+    //                 } else {
+    //                     status = 500;
+    //                     result.status = status;
+    //                     result.error = err;
+    //                 }
+    //                 res.status(status).send(result);
+    //             });
+    //         } else {
+    //             status = 500;
+    //             result.status = status;
+    //             result.error = err;
+    //             res.status(status).send(result);
+    //         }
+    //     });
+    // },
+
     add: (req, res) => {
-        mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
-            let result = {};
-            let status = 201;
-            if (!err) {
-                const { name, password } = req.body;
-                const user = new User({ name, password });
-                user.save((err, user) => {
-                    if (!err) {
-                        result.status = status;
-                        result.result = user;
-                    } else {
-                        status = 500;
-                        result.status = status;
-                        result.error = err;
-                    }
-                    res.status(status).send(result);
-                });
-            } else {
-                status = 500;
-                result.status = status;
-                result.error = err;
-                res.status(status).send(result);
-            }
-        });
+        mongoose.connect(connUri, { useNewUrlParser:  true }, err => {
+            if (err) res.status(500).send(err);
+            const { name, password } = req.body;
+            const user = new User({ name, password });
+            user.save((err, user) => {
+                if (err) res.status(500).send(err);
+                res.status(201).send(user);
+            })
+        })
     },
+
+    // login: (req, res) => {
+    //     const { name, password } = req.body;
+
+    //     mongoose.connect(connUri, { useNewUrlParser: true }, err => {
+    //         let result = {};
+    //         let status = 200;
+    //         if (!err) {
+    //             User.findOne({ name }, (err, user) => {
+    //                 if (!err) {
+    //                     if (user) {
+    //                         bcrypt.compare(password, user.password)
+    //                             .then(match => {
+    //                                 if (match) {
+
+    //                                     // creating token
+    //                                     const payload = { user: user.name };
+    //                                     const options = {
+    //                                         expiresIn: '2d',
+    //                                         issuer: 'https://scotch.io'
+    //                                     };
+    //                                     const secret = process.env.JWT_SECRET;
+    //                                     const token = jwt.sign(payload, secret, options);
+    //                                     // console.log('Token: ', token);
+    //                                     result.token = token;
+    //                                     // result.status = status;
+    //                                     // result.result = user;
+    //                                 } else {
+    //                                     status = 401;
+    //                                     result.status = status;
+    //                                     result.error = 'Authentication Error';
+    //                                 }
+    //                                 res.status(status).send(result);
+    //                             }).catch(err => {
+    //                                 status = 500;
+    //                                 result.status = status;
+    //                                 result.error = err;
+    //                                 res.status(status).send(result);
+    //                             });
+    //                     } else {
+    //                         res.status(404).send({ 'Error': 'User does not exist' });
+    //                     }
+
+    //                 } else {
+    //                     status = 404;
+    //                     res.status = status;
+    //                     result.err = err;
+    //                     res.status(status).send(result);
+    //                 }
+    //             });
+    //         } else {
+    //             status = 500;
+    //             result.status = status;
+    //             result.error = err;
+    //             res.status(status).send(result);
+    //         }
+    //     });
+    // },
 
     login: (req, res) => {
         const { name, password } = req.body;
-
         mongoose.connect(connUri, { useNewUrlParser: true }, err => {
-            let result = {};
-            let status = 200;
-            if (!err) {
-                User.findOne({ name }, (err, user) => {
-                    if (!err) {
-                        if (user) {
-                            bcrypt.compare(password, user.password)
-                                .then(match => {
-                                    if (match) {
+            if (err) res.status(500).send(err);
+            User.findOne({ name }, (err, user) => {
+                if (err) res.status(404).send(err);
+                if (user) {
+                    bcrypt.compare(password, user.password)
+                        .then(match => {
+                            if (!match) res.status(400).send({ 'Error': 'Authentication failed, Invalid password' });
+                            const payload = { user: user.name };
+                            const options = {
+                                expiresIn: '2d',
+                                issuer: 'https://scotch.io'
+                            };
+                            const secret = process.env.JWT_SECRET;
+                            const token = jwt.sign(payload, secret, options);
 
-                                        // creating token
-                                        const payload = { user: user.name };
-                                        const options = {
-                                            expiresIn: '2d',
-                                            issuer: 'https://scotch.io'
-                                        };
-                                        const secret = process.env.JWT_SECRET;
-                                        const token = jwt.sign(payload, secret, options);
-                                        // console.log('Token: ', token);
-                                        result.token = token;
-                                        // result.status = status;
-                                        // result.result = user;
-                                    } else {
-                                        status = 401;
-                                        result.status = status;
-                                        result.error = 'Authentication Error';
-                                    }
-                                    res.status(status).send(result);
-                                }).catch(err => {
-                                    status = 500;
-                                    result.status = status;
-                                    result.error = err;
-                                    res.status(status).send(result);
-                                });
-                        } else {
-                            res.status(404).send({ 'Error': 'User does not exist' });
-                        }
-
-                    } else {
-                        status = 404;
-                        res.status = status;
-                        result.err = err;
-                        res.status(status).send(result);
-                    }
-                });
-            } else {
-                status = 500;
-                result.status = status;
-                result.error = err;
-                res.status(status).send(result);
-            }
-        });
+                            res.status(200).send({ token });
+                        })
+                } else {
+                    res.status(404).send({'Error': 'User not found'});
+                }
+            })
+        })
     },
 
     getAll: (req, res) => {
--- routes/ifs.js
+++ routes/ifs.js
@@ -3,7 +3,7 @@ const validateToken = require('../utils').validateToken;
 
 module.exports = router => {
     router.route('/ifs')
-        .get(validateToken, controller.getAll)
+        .get(validateToken, controller.getAll)  // All routes are protected
         .post(validateToken, controller.add)
         .patch(validateToken, controller.update)
         .delete(validateToken, controller.remove)
